e36efa2b6aefb617d3278d4e8e942d4d75165ca1
fran14arce <farcer01@educantabria.es>
2022-02-14 11:57:24 +0100

    Se añade logo al navbar

--- examendi/src/components/Menu.jsx
+++ examendi/src/components/Menu.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
 import {PaginasApp} from '../data/PaginasApp';
 import { Link } from 'react-router-dom';
+import icon from '../assets/images/logo.png';
 
 class Menu extends React.Component {
     constructor(props) {
@@ -12,7 +13,9 @@ class Menu extends React.Component {
         return (
             <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
                 <Container>
-                    <Navbar.Brand href="#home">Examen desarrollo de interfaces</Navbar.Brand>
+                    <Navbar.Brand href="#home">
+                        <img className="icon_menu" src={icon}></img>
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
